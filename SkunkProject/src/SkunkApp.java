2f3547ed22d21d1848b9c20e8786a05fc4885f99
jwang2 <jwang2@USEDPNB43260.insight.com>
2018-09-30 15:53:31 -0500

    SkunkApp Commit

--- SkunkProject/src/SkunkApp.java
+++ SkunkProject/src/SkunkApp.java
@@ -4,11 +4,14 @@ public class SkunkApp {
 	public static void main(String[] args) {
 		
 		StdOut.println("Welcome to Skunk");
-		Die d=new Die(new int[] {1,2,3,2,1});
-		for (int i=0; i<5;i++){
-			d.roll();
 		
-		StdOut.println(d.getLastRoll());
+		Die die1=new Die(new int[] {1,2,3,2,1});
+		Die die2=new Die(new int[] {2,3,4,2,3});
+		Dice dice1=new Dice(die1, die2);
+		for (int i=0; i<5;i++){
+			die1.roll();
+			die2.roll();
+		StdOut.println(dice1.getLastRoll());
 		}
 	}
 
