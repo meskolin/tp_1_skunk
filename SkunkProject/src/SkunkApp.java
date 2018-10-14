cee6fa40623080ba3c971f2408655bb02d31b476
jwang2 <jwang2@USEDPNB43260.insight.com>
2018-10-14 11:41:16 -0500

    JW's first commit

--- SkunkProject/src/SkunkApp.java
+++ SkunkProject/src/SkunkApp.java
@@ -6,7 +6,7 @@ public class SkunkApp implements Reporter
 
 	public static void main(String[] args)
 	{
-		StdOut.println("Welcome to 635 Skunk project!");
+		StdOut.println("Hello! Welcome to 635 Skunk project!");
 		
 		StdOut.println("Play one Turn for player1");
 		
