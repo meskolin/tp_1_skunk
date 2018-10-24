8dc544ada860826426f936ebfddd6122430e7210
jwang2 <jwang2@USEDPNB43260.insight.com>
2018-10-23 23:46:30 -0500

    multiple players turn commit

--- SkunkProject/src/SkunkApp.java
+++ SkunkProject/src/SkunkApp.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import edu.princeton.cs.introcs.StdIn;
 import edu.princeton.cs.introcs.StdOut;
 
@@ -7,18 +9,27 @@ public class SkunkApp implements Reporter
 	public static void main(String[] args)
 	{
 		StdOut.println("Hello! Welcome to 635 Skunk project!");
+		StdOut.println("Please enter the number of players: ");
+		int numPlayer=StdIn.readInt();
+		ArrayList<String> playerList=new ArrayList<>(numPlayer);
+		for (int i=0; i<numPlayer; i++) {
+			StdOut.println("Please enter Player"+(i+1)+"'s name:");
+			String playerName=StdIn.readLine();
+			playerList.add(playerName);
+			StdOut.println(playerList);
+		}
 		
-		StdOut.println("Play one Turn for player1");
-		
-		String player = "player1";
-		
+		for (int i=0; i<numPlayer;i++) {
+		StdOut.println("Play one Turn for player"+(i+1));
+		String player = "player"+(i+1);
 		Turn turn = new Turn(); 
+		
 		while(true) {
 			StdOut.println("Your current turn score is " + turn.getTurnScore());
 			StdOut.println("Do you wanna roll? y or n (ENTER=>y)");
 			String response=StdIn.readLine();
 		
-			if (!response.equals("n")) {
+			if (response.equals("y")) {
 				turn.rollAgain();
 				turn.scoreTurn();			
 				StdOut.println("You rolled a "+turn.getLastRoll().getDice().getDie1().getLastRoll() + " and a " +
@@ -28,16 +39,21 @@ public class SkunkApp implements Reporter
 					StdOut.println("Your turn is over :( ");
 					break;
 				}
-			}	
-			else {
+			}
+			else if (response.equals("n")) {
 				StdOut.println("You declined a roll.");
+				break;
+			}
+			else {
+				StdOut.println("You entered an invalid response.");
 				break;
 			}
 		}
 		int turnScore=turn.getTurnScore();
-		StdOut.println("You scored: "+turnScore+" for this turn.");
-	}
+		StdOut.println("Player"+(i+1)+ " scored: "+turnScore+" for this turn.");
+		}
 	
+	}
 	@Override
 		public void showMessage(String msg) 
 		{
